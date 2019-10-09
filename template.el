8717a81df00763f6ac08665bb747f1f7b084886e
Simon Härer <simon.haerer@active-group.de>
2019-10-09 16:18:36 +0200

    Add mathjax for local usage

--- template.el
+++ template.el
@@ -1,12 +1,22 @@
+
+(defvar my-directory (if load-file-name
+                         ;; File is being loaded.
+                         (file-name-directory load-file-name)
+                       ;; File is being evaluated using, for example, `eval-buffer'.
+                       default-directory))
+
 (defun make-presentation ()
   (interactive)
-  (let ((title (read-string "Title: ")))
+  (let ((title (read-string "Title: "))
+        (mathjax? (read-string "Use mathjax locally? [y/N]")))
     (switch-to-buffer (concat title ".org"))
     (set-visited-file-name (concat title ".org"))
     (org-mode)
     (insert "#+title: " title "\n")
     (insert "#+author: " user-full-name "\n")
-    (insert "#+REVEAL_PLUGINS: (notes)\n")
+    (insert "#+REVEAL_PLUGINS: (notes)\n") 
+    (when (string= "y" mathjax?)
+       (insert "#+REVEAL_MATHJAX_URL: file://" my-directory "mathjax/tex-chtml.js\n")) 
     (insert "#+REVEAL_THEME: active\n")
     (insert "#+REVEAL_HLEVEL: 100\n")
     (insert "#+REVEAL_TRANS: none\n")
