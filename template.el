a2cf680f1e0c6dc62dd4ca1f1a7c8f926e745da4
Simon Härer <simon.haerer@active-group.de>
2019-10-09 13:32:13 +0200

    Add auto path

--- template.el
+++ template.el
@@ -1,10 +1,18 @@
+(defvar my-directory (if load-file-name
+                         ;; File is being loaded.
+                         (file-name-directory load-file-name)
+                       ;; File is being evaluated using, for example, `eval-buffer'.
+                       default-directory))
+                       
 (defun make-presentation ()
   (interactive)
   (let ((title (read-string "Title: ")))
     (switch-to-buffer (concat title ".org"))
+    (set-visited-file-name (concat title ".org"))
     (org-mode)
     (insert "#+title: " title "\n")
     (insert "#+author: " user-full-name "\n")
+    (insert "#+REVEAL_ROOT: file://" my-directory " \n")
     (insert "#+REVEAL_PLUGINS: (notes)\n")
     (insert "#+REVEAL_THEME: active\n")
     (insert "#+REVEAL_HLEVEL: 100\n")
